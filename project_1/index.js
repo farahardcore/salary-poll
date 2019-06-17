4eb0837ae80f5292ba09b7fc85910c7e89c9f585
Farid <farahardcore161@gmail.com>
2019-06-17 11:55:29 +0300

    Add work with cycles and conditions

--- project_1/index.js
+++ project_1/index.js
@@ -9,13 +9,71 @@ const appData = {
     income : [],
     savings : false
 };
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    a2 = prompt("Во сколько обойдется?", ""),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    a4 = prompt("Во сколько обойдется?", "");
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
-// console.log(typeof(+appData.cash));
-alert(appData.cash / 30);
+for (let i=0; i < 2;i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+        if( (typeof(a))==="string" && (typeof(a) != null) && (typeof(b) != null)
+        && a != "" && b != "" && a.length<50){
+            console.log("done");
+            appData.expenses[a] = b;
+        }else{
+            console.log("Bad result");
+            i--;
+        }
+};
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
+
+appData.moneyPerDay = appData.cash / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+if(appData.moneyPerDay < 100){
+    console.log("Низкий уровень дохода");
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log("Средний уровень дохода");
+}else if(appData.moneyPerDay > 2000){
+    console.log("Высокий уровень дохода");
+}else{
+    console.log("Произошла Ошибка")
+};
 
 
